4af02561ffbc0d55abb7041a37822f62fa7801c3
sk naim <sknaimuddin234@gmail.com>
2023-03-23 20:35:33 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 sudo apt install wget
 wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/ffmpeg/7:5.1.1-1ubuntu2.1/ffmpeg_5.1.1.orig.tar.xz
 tar -xf ffmpeg_5.1.1.orig.tar.xz
-cd ffmpeg_5.1.1
+cd ffmpeg-5.1.1
 ./configure
 make
 sudo make install
